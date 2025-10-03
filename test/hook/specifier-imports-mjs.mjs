6e3fe6afd27c1b231029510b99fa9d9ed7425ff3
pabloerhard <pabloerhard02@gmail.com>
2025-10-03 10:58:45 -0400

    Fixed issues when nested folders and added test for specifier and esm import.

--- test/hook/specifier-imports-mjs.mjs
+++ test/hook/specifier-imports-mjs.mjs
@@ -1,9 +1,8 @@
-import { foo } from '../fixtures/nested-folder/specifier.js'
+import { foo } from '../fixtures/specifier.mjs'
 import Hook from '../../index.js'
 import { strictEqual } from 'assert'
-
 Hook((exports, name) => {
-  if (name.endsWith('fixtures/nested-folder/specifier.js')) {
+  if (name.endsWith('fixtures/specifier.mjs')) {
     exports.foo = 1
   }
 })
